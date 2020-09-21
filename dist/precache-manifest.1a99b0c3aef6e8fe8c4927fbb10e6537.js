678b513c1db7c82cd6e9ff31b6e679360f68fcce
jake-freeman <Jacob.Freeman@vorne.com>
2020-09-20 23:15:12 -0500

    Add prod dist

---
+++ dist/precache-manifest.1a99b0c3aef6e8fe8c4927fbb10e6537.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "018a4497a309f511c1a592426c258d58",
+    "url": "/trivia-db-sandbox/index.html"
+  },
+  {
+    "revision": "6fece50e70467d035374",
+    "url": "/trivia-db-sandbox/static/js/2.1bf0d1b8.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/trivia-db-sandbox/static/js/2.1bf0d1b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc9bcb3bc51d26ee0cc1",
+    "url": "/trivia-db-sandbox/static/js/main.a2e54580.chunk.js"
+  },
+  {
+    "revision": "72ee8a625277611c15a7",
+    "url": "/trivia-db-sandbox/static/js/runtime-main.6bf379cc.js"
+  }
+]);
